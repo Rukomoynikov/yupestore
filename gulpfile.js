9ce68c10df52660b81654b11e0056d37aba409ac
Maksim Rukomoynikov <cat@mos-it.com>
2015-07-10 12:52:39 +0300

    Разделил большой CSS на несколько маленьких, дописал комментарии к блокам

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,70 @@ var gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
 	babel = require('gulp-babel'),
 	autoprefixer = require('/Users/air/node_modules/autoprefixer/'),
-	livereload = require('gulp-livereload');
+	livereload = require('gulp-livereload'),
+	spritesmith = require('gulp.spritesmith')
+	sftp = require('gulp-sftp'),
+	connect = require('gulp-connect');
 
-gulp.task('css', function(){
+gulp.task('sprite', function(){
+	return gulp.src(['./assets/images/icon-heart.png', './assets/images/icon-compare.png', 
+		'./assets/images/sandwich-button.png', './assets/images/arrow-to-bottom.png', 
+		'./assets/images/icon-search.png', './assets/images/rateback.png',
+		'./assets/images/icon-correct.png', './assets/images/icon-incorrect.png'])
+	.pipe(spritesmith({
+		imgName : "sprite.png",
+		cssName : "sprite.css"
+	}))
+	.pipe(gulp.dest('assets/images/sprite'));
+});
+
+gulp.task('stylus', function(){
 	var processors = [
 		autoprefixer({browsers: ['last 1 version', 'IE 8', 'IE 9', 'IE 10', 'IE 11']}),
 	]
 	return gulp.src('./assets/css/*.styl')
-		.pipe(stylus())
+		.pipe(stylus({
+			// compress: true
+			// linenos: true
+			'include css': true
+		}))
 		.pipe(postcss(processors))
 		.pipe(gulp.dest('./assets/css/'))
 		.pipe(livereload())
+		.pipe(connect.reload());
 });
 
 gulp.task('jade', function(){
-	return gulp.src('./*.jade')
-		.pipe(jade())
+	return gulp.src(['./*.jade'])
+		.pipe(jade({
+			pretty: true
+		}))
 		.pipe(gulp.dest('./'))
+		.pipe(livereload())
+		.pipe(connect.reload());
 });
 
 gulp.task('watch', function(){
-	livereload.listen({
-		port : 8080,
-		basePath : '/',
-		host : 'localhost'
-	})
+	livereload.listen();
 	gulp.watch('./*.jade', ['jade']);
-	gulp.watch('./assets/css/*.styl', ['css']);
+	gulp.watch('./assets/css/*.styl', ['stylus']);
 })
 
-gulp.task('default', ['watch', 'css', 'jade'])
+gulp.task('connect', function(){
+	connect.server({
+		root : './',
+		livereload : 'true'
+	})
+})
+
+gulp.task('sftp', function () {
+    return gulp.src('./**')
+        .pipe(sftp({
+            host: 'ftp.selcdn.ru',
+            user: '9025',
+            pass: 'y2E0LWqpXQ',
+            remotePath : '/justtest/yupestore'
+        }));
+});
+
+gulp.task('default', ['connect','watch', 'stylus', 'jade'])

c615544e8427d2aba0d43088355b3a839bf6ce40
Maksim Rukomoynikov <cat@mos-it.com>
2015-08-04 14:47:35 +0300

    Fix #14 Fix #16

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).on('click',function(){
-	$('.collapse').collapse('hide');
+	$('#mainmenu.collapse').collapse('hide');
 })
 
 $(document).on('keydown',function(key){
@@ -10,8 +10,25 @@ $(document).on('keydown',function(key){
 
 $(function(){
 
+	// Выбор города на экране офомления заказа.
 	$('li.choosecity').on('click', function(elem){
 		$('.opencitylist').text($(this).text());
-	})
+	});
+    
+    // $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    //   " - $" + $( "#slider-range" ).slider( "values", 1 ) );	
 
 })
+
+ $(function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $('#type0_name').val(ui.values[ 0 ]);
+        $('#type0_name_2').val(ui.values[ 1 ]);
+      }
+    });
+  });
